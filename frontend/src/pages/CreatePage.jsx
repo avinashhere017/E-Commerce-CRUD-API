5237d14ceeb3b01e569f6798c5985321a5ac4400
avinashhere017 <avinash170820@gmail.com>
2023-10-11 13:54:07 +0530

    frontend_done

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,8 @@
-import axios from "axios";
 import { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-
+import { VITE_BACKEND_URL } from "../App";
 
 const CreatePage = () => {
 
@@ -15,22 +15,21 @@ const CreatePage = () => {
 
     const saveProduct = async(e) => {
         e.preventDefault();
-        if(name === "" || quantity === "" || price === "" || image == ""){
-            toast.error('Please fill out all input completely');
+        if(name === "" || quantity === "" || price === "" || image === ""){
+            alert('Please fill out all input completely');
             return;
         }
-        try {
+        try{
             setIsLoading(true);
-            const response = await axios.post("https://e-commerce-crud-api.onrender.com/api/products", {name: name, quantiy: quantity, price: price, image: image});
-            toast.success(`Save ${response.data.name} Successfully`);
+            const response = await axios.post(`${VITE_BACKEND_URL}/api/products`, {name: name, quantity: quantity, price: price, image: image});
+            toast.success(`Save ${response.data.name} sucessfully`);
             setIsLoading(false);
             navigate("/");
-        } catch (error) {
+        }catch (error){
             toast.error(error.message);
             setIsLoading(false);
         }
     }
-
 
     return (
         <div className="max-w-lg bg-white shadow-lg mx-auto p-7 rounded mt-6">
@@ -40,29 +39,29 @@ const CreatePage = () => {
             <form onSubmit={saveProduct}>
                 <div className="space-y-2">
                     <div>
-                        <label className="text-gray-600 mb-2 block font-semibold">Name</label>
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="w-full block border p-3 text-gray-600  rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Name" />
+                        <label>Name</label>
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Name" />
                     </div>
                     <div>
-                        <label className="text-gray-600 mb-2 block font-semibold">Quantity</label>
-                        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} className="w-full block border p-3 text-gray-600  rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Quantity" />
+                        <label>Quantity</label>
+                        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Quantity" />
                     </div>
                     <div>
-                        <label className="text-gray-600 mb-2 block font-semibold">Price</label>
-                        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} className="w-full block border p-3 text-gray-600  rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Price" />
+                        <label>Price</label>
+                        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Price" />
                     </div>
                     <div>
-                        <label className="text-gray-600 mb-2 block font-semibold">Image URL</label>
-                        <input type="text" value={image} onChange={(e) => setImage(e.target.value)} className="w-full block border p-3 text-gray-600  rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Image URL" />
+                        <label>Image URL</label>
+                        <input type="text" value={image} onChange={(e) => setImage(e.target.value)} className="w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder-gray-400" placeholder="Enter Image URL" />
                     </div>
                     <div>
-                        { !isLoading &&  (<button className="block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer">Save</button>)}
-                        
+                        { !isLoading && ( <button className="block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer">Save</button>)}         
                     </div>
                 </div>
             </form>
         </div>
-    );
+    )
 }
+
 
 export default CreatePage;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,70 +1,66 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import Product from "../components/Product"
 import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
-import Product from "../components/Product";
-import TableProduct from "../components/TableProduct";
+import { VITE_BACKEND_URL } from "../App";
 
 const HomePage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
-    try {
-      setIsLoading(true);
-      const response = await axios.get("https://e-commerce-crud-api.onrender.com/api/products");
-      console.log(response.data)
-      setProducts(response.data);
-      setIsLoading(false);
-    } catch (error) {
-      toast.error(error.message);
-      setIsLoading(false);
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getProducts = async () => {
+        try{
+           setIsLoading(true); 
+           const response = await axios.get(`${VITE_BACKEND_URL}/api/products/`);
+           console.log(response.data);
+           setProducts(response.data);
+           setIsLoading(false);
+
+        } catch (error){
+           console.log(error);
+        }
     }
-  };
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+    useEffect(() => {
+        getProducts();
+    }, [])
 
-  return (
-    <div>
-      <div>
-        <Link
-          to="/create"
-          className="inline-block mt-4 shadow-md bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer"
-        >
-          Create a Product
-        </Link>
-      </div>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
-        {isLoading ? (
-          "Loading"
-        ) : (
-          <>
-            {products.length > 0 ? (
-              <>
-                {products.map((product, index) => {
-                  return (
-                    <Product
-                      key={index}
-                      product={product}
-                      getProducts={getProducts}
-                    />
-                  );
-                })}
-              </>
-            ) : (
-              <div className="mt-4 bg-gray-800 text-white font-serif p-4">
-                There is no product
-              </div>
-            )}
-          </>
-        )}
-      </div>
+    return (
+        <div>
+            <div>
+                <Link to="/create" className="inline-block mt-4 shadow-md bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer">
+                    Create a Product
+                </Link>
+            </div>
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
+                {isLoading ? (
+                    "Loading"
+                ) : (
+                    <>
+                    {products.length > 0 ? (
+                        <>
 
-      <TableProduct products={products} />
-    </div>
-  );
-};
+                            {
+                                products.map((product, index) => {
+                                   return (
+                                     <Product key={index} product={product} getProducts={getProducts}/>
+                                   )
+                                })
+                            }
+                        </>
+                    ) : (
+                        <div>
+                            There is no product
+                        </div>
+                    )}
+                    
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+
 
 export default HomePage;
